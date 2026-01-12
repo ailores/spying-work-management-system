154f48ee95f93bf34e058915b9c2517a5dd73924
ailored <olia.derkach2015@gmail.com>
2026-01-12 15:40:35 +0200

    :bug: fix: add a listener to database.py to properly delete a mission with its targets

--- app/core/db/database.py
+++ app/core/db/database.py
@@ -1,13 +1,21 @@
-from sqlalchemy import create_engine, Engine
+from sqlalchemy import create_engine, event
+from sqlalchemy.engine import Engine
 from sqlalchemy.orm import sessionmaker, Session
 
 from app.config.config import settings
-from app.core.models import Base
+from app.core.models.models import Base
 
 engine: Engine = create_engine(
     url=settings.DB_URL,
     echo=True
 )
+
+
+@event.listens_for(Engine, "connect")
+def enable_sqlite_fk(dbapi_connection, connection_record) -> None:
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 
 def create_db_and_tables() -> None:
